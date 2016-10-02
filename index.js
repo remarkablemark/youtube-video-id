0a5ddb4e540a4208180bd6469ffce8d9e5b1b6b6
Mark <mark@remarkablemark.org>
2016-10-02 13:35:58 -0400

    Create index.js with the main module `getYouTubeVideoId()`
    Currently, it assumes the first argument is an ID and
    simply returns that.

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Get the YouTube video ID from a url or string.
+ *
+ * @param  {String} string - The url or string.
+ * @return {String}        - The video ID.
+ */
+function getYouTubeVideoId(string) {
+    return string;
+}
+
+module.exports = getYouTubeVideoId;
